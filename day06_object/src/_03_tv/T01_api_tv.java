071cd8937731fe02f88054361b52c062d38a4b5b
professorjiwon <professorjiwon@gmail.com>
2024-05-14 17:54:36 +0900

    day06_tv

---
+++ day06_object/src/_03_tv/T01_api_tv.java
@@ -0,0 +1,30 @@
+package _03_tv;
+
+public class T01_api_tv {
+	String company = "LG";
+	String model = "울트라 FULL HD";
+	int inch = 65;
+	int volume;		// 기본값 : 0
+	int channel = 1;
+	boolean power;  // 기본값 : false
+	
+	void power() {
+		power = !power;
+		if(power)
+			System.out.println("tv를 켭니다");
+		else
+			System.out.println("tv를 끕니다");
+	}	
+	int channelUp() {
+		channel++;
+		return channel;
+	}
+	int channelDown() {
+		channel--;
+		return channel;
+	}
+	int volume(int volumeInput) {
+		volume = volumeInput;
+		return volume;
+	}
+}
---
+++ day06_object/src/_03_tv/T01_main_tv.java
@@ -0,0 +1,21 @@
+package _03_tv;
+
+public class T01_main_tv {
+
+	public static void main(String[] args) {		
+		T01_api_tv tv1 = new T01_api_tv();
+		System.out.println("회사명 : " + tv1.company);
+		System.out.println("모델명 : " + tv1.model);
+		System.out.println(tv1.inch + "인치");
+
+		tv1.power();
+		// System.out.println("tv상태 : " + tv1.power);
+		System.out.println("현재 채널 : " + tv1.channelUp());
+		System.out.println("현재 채널 : " + tv1.channelUp());
+		System.out.println("현재 채널 : " + tv1.channelDown());
+		System.out.println("볼륨 : " + tv1.volume(12));
+		tv1.power();
+		// System.out.println("tv상태 : " + tv1.power);
+	}
+
+}
