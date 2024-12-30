843f083c6a310150dfd61d3819dd32f68a76aa61
professorjiwon <professorjiwon@gmail.com>
2024-12-30 12:47:21 +0900

    12.30_4교시

--- chap02_variable/src/chap02_variable/Report_08.java
+++ chap02_variable/src/chap02_variable/Report_08.java
@@ -7,12 +7,18 @@ public class Report_08 {
 		// 사용자로 부터 국어, 수학, 컴퓨터의 점수를 입력받아
 		// 총점과 평균(소수점2째자리까지(반올림)) 출력
 		Scanner sc = new Scanner(System.in);
-		
+		/*
 		System.out.print("국어점수 입력 >> ");
 		int kor = sc.nextInt();
 		System.out.print("수학점수 입력 >> ");
 		int math = sc.nextInt();
 		System.out.print("컴퓨터점수 입력 >> ");
+		int com = sc.nextInt();
+		*/
+		
+		System.out.print("점수 입력(국어,수학,컴퓨터점수를 띄어쓰기로 넣기) >> ");
+		int kor = sc.nextInt();
+		int math = sc.nextInt();
 		int com = sc.nextInt();
 		
 		int sum = kor + math + com;
@@ -21,5 +27,4 @@ public class Report_08 {
 		System.out.println("총점 : " + sum);
 		System.out.printf("평균 : %.2f", avg);
 	}
-
 }
