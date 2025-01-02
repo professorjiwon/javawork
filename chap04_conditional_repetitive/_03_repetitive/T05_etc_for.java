1c9d1e262fed6826d82cc55a5e888af5728a3800
professorjiwon <professorjiwon@gmail.com>
2025-01-02 16:45:51 +0900

    01.02_6교시_random

---
+++ chap04_conditional_repetitive/_03_repetitive/T05_etc_for.java
@@ -0,0 +1,57 @@
+package _03_repetitive;
+
+public class T05_etc_for {
+
+	public static void main(String[] args) {
+		// 같은 자료형 일때
+		int num1, num2, num3;
+		num1= 10;
+		num2=10;
+		num3=10;
+		
+		int num4=10, num5=10, num6=10;
+		num4 = num5 = num6 = 100;
+		
+		// for문에서 여러개의 변수 사용가능
+		// ** 주의 : 조건식은 반드시 변수가 하나만 들어가야 한다
+		for(int i=1, j=30; i<=10; i++, j--) {
+			System.out.println("i=" + i + ", j=" + j);
+		}
+		
+		// for(;;) : while(true)와 동일
+		int num7 = 10;
+		for(;;) {
+			System.out.println(num7);
+			
+			if(num7 == 15)
+				break;
+			
+			num7++;
+		}
+		System.out.println("--------------------");
+		
+		// for문에 이름을 달아줄 수 있다
+		loop : for(int dan=2; dan<=9; dan++) {
+			for(int i=1; i<=9; i++) {
+				System.out.println(dan + "*" + i + "=" + dan*i);
+				if(i==5)
+					break loop;
+			}
+			System.out.println();
+		}
+		System.out.println("구구단 종료");
+		
+		
+		outer : for(int dan=2; dan<=9; dan++) {
+			inner : for(int i=1; i<=9; i++) {
+				for(int z=1; z<=5; z++) {
+					System.out.println(dan + ", " + i + ", " + z);
+					if(z==3)
+						break inner;
+				}
+			}
+			System.out.println("inner for문 종료");
+		}
+		System.out.println("outer for문 종료");
+	}
+}
---
+++ chap04_conditional_repetitive/_03_repetitive/T06_math_random.java
@@ -0,0 +1,31 @@
+package _03_repetitive;
+
+public class T06_math_random {
+
+	public static void main(String[] args) {
+		// Math(수학에 관련된 클래스)
+		// random : 실수형으로 출력(0 ~ 0.99999999999)
+		double random1 = Math.random();
+		System.out.println(random1);
+		
+		double random2 = Math.random() * 10; // 0 ~ 9.999999999
+		System.out.println(random2);
+		
+		// 0~9까지 정수만 출력
+		int random3 = (int)(Math.random() * 10);
+		System.out.println(random3);
+		
+		// 1~10까지 정수만 출력
+		int random4 = (int)(Math.random() * 10) + 1;
+		System.out.println(random4);
+		
+		// 0~4까지 정수만 출력
+		int random5 = (int)(Math.random() * 5);
+		System.out.println(random5);
+		
+		// 1~5까지 정수만 출력
+		int random6 = random5 + 1;
+		System.out.println(random6);
+	}
+
+}
