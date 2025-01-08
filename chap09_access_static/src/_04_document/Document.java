d5a2ab2a70729aa20763248a9371db1391615852
professorjiwon <professorjiwon@gmail.com>
2025-01-08 18:03:39 +0900

    01.08_7교시_document_static

---
+++ chap09_access_static/src/_04_document/Document.java
@@ -0,0 +1,14 @@
+package _04_document;
+
+public class Document {
+	String name;  // 인스턴스 변수
+	static int count;	// 정적 변수
+	
+	Document() {
+		System.out.println("제목없음"+ ++count + ".txt 문서가 생성됨");
+	}
+	Document(String name) {
+		this.name = name;
+		System.out.println( this.name + ".txt 문서가 생성됨");
+	}
+}
---
+++ chap09_access_static/src/_04_document/Document_main.java
@@ -0,0 +1,13 @@
+package _04_document;
+
+public class Document_main {
+
+	public static void main(String[] args) {
+		Document d1 = new Document();		// 제목없음1.txt
+		Document d2 = new Document("자바");	// 자바.txt
+		Document d3 = new Document();		// 제목없음2.txt
+		Document d4 = new Document("오라클");	// 오라클.txt
+		Document d5 = new Document("HTML");	// HTML.txt
+		Document d6 = new Document();		// 제목없음3.txt
+	}
+}
