61bf48d72a9c5c85d072c249a2d848369b4db86b
professorjiwon <professorjiwon@gmail.com>
2025-01-10 11:21:47 +0900

    01.10_2교시_다형성

--- chap10_polymorphism/src/_02_polyBuyer/Buyer.java
+++ chap10_polymorphism/src/_02_polyBuyer/Buyer.java
@@ -1,39 +1,47 @@
 package _02_polyBuyer;
 
 public class Buyer {
-	int money = 500;
-	int bonusPoint;
+	int money = 500; // 225
+	int bonusPoint;  // 13 
 
-	/*  제품의 갯수만큼 오버로딩을 해야함
-	void buy(Computer com) {
-		if(money < p.price) {
-			System.out.println("잔액부족");
-			return;
-		}
-		money -= p.price;
-		bonusPoint += p.bonusPoint;
-		System.out.println(p + "를 구입함");
-	}
-	void buy(Tv tv) {
-		if(money < p.price) {
-			System.out.println("잔액부족");
-			return;
-		}
-		money -= p.price;
-		bonusPoint += p.bonusPoint;
-		System.out.println(p + "를 구입함");
-	}
-	void buy(GameDevice gd) {
-		if(money < p.price) {
-			System.out.println("잔액부족");
-			return;
-		}
-		money -= p.price;
-		bonusPoint += p.bonusPoint;
-		System.out.println(p + "를 구입함");
-	}
+	/*
+	  - 매개변수로 객체를 받음. 
+	    반환형 메서드이름(클래스명 변수명)  
+	       -> 인자값은 반드시 클래스명으로 생성된 객체만 넘겨줘야 한다
+			  그러면 객체의 주소가 넘어와서 변수명에 저장됨
 	*/
 	
+	/*
+	//  제품의 갯수만큼 만들어야 함
+	void buy(Computer com) {
+		if(money < com.price) {
+			System.out.println("잔액부족");
+			return;
+		}
+		money -= com.price;
+		bonusPoint += com.bonusPoint;
+		System.out.println(com + "를 구입함");
+	}
+	void buy(Tv tv) {
+		if(money < tv.price) {
+			System.out.println("잔액부족");
+			return;
+		}
+		money -= tv.price;
+		bonusPoint += tv.bonusPoint;
+		System.out.println(tv + "를 구입함");
+	}
+	void buy(GameDevice gd) {
+		if(money < gd.price) {
+			System.out.println("잔액부족");
+			return;
+		}
+		money -= gd.price;
+		bonusPoint += gd.bonusPoint;
+		System.out.println(gd + "를 구입함");
+	}
+	*/
+
 	// 형변환을 하면 모든 제품을 다 받을 수 있다
 	void buy(Product p) {
 		if(money < p.price) {
@@ -45,3 +53,21 @@ public class Buyer {
 		System.out.println(p + "를 구입함");
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- chap10_polymorphism/src/_02_polyBuyer/Product_main.java
+++ chap10_polymorphism/src/_02_polyBuyer/Product_main.java
@@ -4,14 +4,18 @@ public class Product_main {
 
 	public static void main(String[] args) {
 		Buyer b = new Buyer();
-		b.buy(new Tv());
+
 		b.buy(new GameDevice());
 		b.buy(new Computer());
+		b.buy(new Tv());
 		System.out.println("현재 남은 돈 : " + b.money + "만원");
 		System.out.println("보너스 점수 : " + b.bonusPoint + "점");
 		
 		b.buy(new Tv());
-
 	}
-
 }
+
+
+
+
+
