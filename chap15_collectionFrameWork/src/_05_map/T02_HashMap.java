6e0aa56fe19903c591a4f3a512558555b2bb3b03
professorjiwon <professorjiwon@gmail.com>
2025-01-17 16:10:02 +0900

    01.17_6교시_hashMap

--- chap15_collectionFrameWork/src/_05_map/T02_HashMap.java
+++ chap15_collectionFrameWork/src/_05_map/T02_HashMap.java
@@ -1,6 +1,7 @@
 package _05_map;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class T02_HashMap {
 
@@ -16,7 +17,52 @@ public class T02_HashMap {
 		// id가 있으면 pw를 보고
 		// pw일치 하면 '로그인 되었습니다'
 		// pw일치 하지 않으면 '비밀번호가 일치하지 않습니다'
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("id 입력 : ");
+		String id = sc.next();
+		
+		if(map.containsKey(id)) {
+			System.out.print("pw 입력 : ");
+			String pw = sc.next();
+			
+			if(pw.equals(map.get(id))) {
+				System.out.println("로그인 되었습니다");
+			} else {
+				System.out.println("비밀번호가 일치하지 않습니다");
+			} 
+		} else {
+			System.out.println("존재하지 않는 id입니다");
+		}
+		
+		
+		/*
+		boolean flag = false;
+		while(true) {
+			System.out.print("id 입력 : ");
+			String id = sc.next();
+			
+			if(map.containsKey(id)) {
+				while(true) {
+					System.out.print("pw 입력 : ");
+					String pw = sc.next();
+					
+					if(pw.equals(map.get(id))) {
+						System.out.println("로그인 되었습니다");
+						flag = true;
+						break;
+					} else {
+						System.out.println("비밀번호가 일치하지 않습니다");
+					}
+				}
+				if(flag)
+					break;
+			} else {
+				System.out.println("존재하지 않는 id입니다");
+				continue;
+			}	
+		}
+		*/
 	}
-
 }
